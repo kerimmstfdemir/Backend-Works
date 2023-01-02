a53b1d3df139ec5bd13102864fd67118b88ff0be
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-02 22:03:21 +0300

    created urls, views, serializers

---
+++ Session 10 - Blog App/blog/views.py
@@ -0,0 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
+
+class CategoryView(ModelViewSet):  # CRUD
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
