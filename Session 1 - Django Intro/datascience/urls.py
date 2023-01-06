ddcba2eb14293c98226d82402401672995ee74b0
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-06 13:03:28 +0300

    defined urls

--- Session 1 - Django Intro/datascience/urls.py
+++ Session 1 - Django Intro/datascience/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homedj
+from .views import homeds
 
 urlpatterns = [
-   path("", homedj),
+   path("", homeds)
 ]
--- Session 1 - Django Intro/main/urls.py
+++ Session 1 - Django Intro/main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # from newproject.views import homedj
 # from datascience.views import homeds
 
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # path("dj/", homedj),
     # path("ds/",homeds),
-    path("dj")
+    path("dj/", include("newproject.urls")),
+    path("ds/", include("datascience.urls")),
 ]
