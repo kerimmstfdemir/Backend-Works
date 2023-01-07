be1ef1d4b35c2ece1ec891b1e5a69e8679ee5c09
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-07 12:47:31 +0300

    added new test function for staff user

--- Session 14 - Test/flight/tests/test_flight_api.py
+++ Session 14 - Test/flight/tests/test_flight_api.py
@@ -1,16 +1,41 @@
 from django.urls import reverse
+from django.contrib. auth.models import User
 
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
+
 from flight.views import FlightView
+from flight.models import Flight
 
 class FlightTestCase(APITestCase):
     
     def setUp(self):
         self.factory = APIRequestFactory()
+        self.flight = Flight.objects.create(
+            flight_number='123ABC',
+            operation_airlines='THY',
+            departure_city='Adana',
+            arrival_city='Ankara',
+            date_of_departure='2023-01-08',
+            etd='08:35:42',
+        )
 
-    def test_flight_lis_as_non_auth_user(self):
+        self.user = User.objects.create_user(
+            username='admin',
+            password='Aa654321*'
+        )
+
+    def test_flight_list_as_non_auth_user(self):
         request = self.factory.get('/flight/flights')  # reverse('flights-list')
-        print(reverse("flights-list"))
+        # print(reverse("flights-list"))
         response = FlightView.as_view({'get':'list'})(request)
         print(response)
         self.assertEquals(response.status_code, 200)
+
+    def test_flight_list_as_staff_user(self):
+        request = self.factory.get('/flight/flights')
+        self.user.is_staff=True
+        self.user.save()
+        force_authenticate(user=self.user)
+        request.user = self.user
+        response = FlightView.as_view({'get':'list'})(request)
+        self.assertEqual(response.status_code, 200)
