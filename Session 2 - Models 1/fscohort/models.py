412ceed6c6b89c624c86b292bc91828b68d29a02
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-09 21:59:51 +0300

    session2: created student table

--- Session 2 - Models 1/fscohort/models.py
+++ Session 2 - Models 1/fscohort/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField(default=1111)
+    about = models.TextField(blank=True, null=True)
+    register = models.DateTimeField(auto_now_add=True)  #? auto_now_add: take the creation date and time
+    last_updated_date = models.DateTimeField(auto_now=True)  #? auto_now: take the update date and time
+    is_active = models.BooleanField()
