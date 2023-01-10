2ab2d86ca6a830a62fc985fddff8ad820dd316ed
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-10 15:32:13 +0300

    session3: updated model

--- Session 3 - Models 2/fscohort/models.py
+++ Session 3 - Models 2/fscohort/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    first_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    number = models.PositiveSmallIntegerField(blank=True)
+    number = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.number} - {self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ('-number',)
+        verbose_name = "Öğrenci",
+        verbose_name_plural = "Öğrenciler"
+        # db_table = "Students_db"  # Changes db name
