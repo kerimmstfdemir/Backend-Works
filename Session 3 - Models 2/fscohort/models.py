c2c7c014d69334ce57528fab2da4c36bbd751c46
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-10 16:13:06 +0300

    session3: uploaded student table

--- Session 3 - Models 2/fscohort/models.py
+++ Session 3 - Models 2/fscohort/models.py
@@ -8,7 +8,8 @@ class Student(models.Model):
     about = models.TextField(blank=True)
     email = models.EmailField(blank=True)
     is_active = models.BooleanField(default=True)
-    avatar = models.ImageField(blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to='student')
+    files = models.FileField(blank=True, null=True, upload_to="student_files")
     register_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
