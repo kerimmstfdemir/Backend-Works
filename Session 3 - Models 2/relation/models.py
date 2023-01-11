3c77bab220b37d37510a666a024d0039985e8444
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-11 16:14:15 +0300

    session3: created oneToMany relational model

--- Session 3 - Models 2/relation/models.py
+++ Session 3 - Models 2/relation/models.py
@@ -6,3 +6,9 @@ class Profile(models.Model):
     image = models.ImageField(upload_to="profile", blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+class Address(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=20)
+    phone = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
