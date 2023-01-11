e0796c7e4d56f0ad498bc29c6a4b7c9607ba35ec
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-11 16:10:58 +0300

    session3: created oneToOne relational model

--- Session 3 - Models 2/relation/models.py
+++ Session 3 - Models 2/relation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to="profile", blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
