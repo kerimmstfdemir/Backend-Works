5b7cd935d3a7fd9e4e951550ea464f3a614538b6
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-11 16:17:53 +0300

    session3: created manyToMany relational model

--- Session 3 - Models 2/relation/models.py
+++ Session 3 - Models 2/relation/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#! One-To-One
 class Profile(models.Model):
     bio = models.TextField(blank=True)
     image = models.ImageField(upload_to="profile", blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+#! One-To-Many
 class Address(models.Model):
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=150)
     city = models.CharField(max_length=20)
     phone = models.PositiveIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#! Many-To-Many
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ManyToManyField(User)
