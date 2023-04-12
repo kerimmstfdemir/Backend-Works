6f4d1bd37567132da210edb7aba173a99f485bb0
kerimmstfdemir <kerimmstfdemir@gmail.com>
2023-04-12 17:14:54 +0300

    added variables work for php

---
+++ Php/a. Php Variables/a.what-is-variable.php
@@ -0,0 +1,9 @@
+<?php
+    $productA = 7000;
+    $productB = 6000;
+    $percentage = 0.2;
+
+    echo $productA + ($productA * $percentage);
+    echo "<br>";
+    echo $productB + ($productB * $percentage);
+?>
