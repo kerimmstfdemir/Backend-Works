f9394738e1f2357dec8d1017b1d3fee03a4215aa
kerimmstfdemir <kerimmstfdemir@gmail.com>
2023-04-18 11:52:34 +0300

    simple static page design has been made for the blog-app

---
+++ Php/a. Php Variables/d. blog-app/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Blog App</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-3">
+                <ul class="list-group">
+                    <li class="list-group-item">Category 1</li>
+                    <li class="list-group-item">Category 2</li>
+                    <li class="list-group-item">Category 3</li>
+                    <li class="list-group-item">Category 4</li>
+                </ul>
+            </div>
+            <div class="col-9">
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="img-fluid" src="./img/1.jpeg" alt="">
+                        </div>
+                        <div class="col-9">
+                            <div class="card-body">
+                                <h5 class="card-title">Film Header</h5>
+                                <p class="card-text">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora architecto
+                                    recusandae nisi culpa? Pariatur, eius labore. Illo odio iste perferendis rem
+                                    exercitationem, voluptatibus, nisi cum porro praesentium blanditiis voluptas harum.
+                                </p>
+                                <div>
+                                    <span class="badge bg-primary">105 Comment</span>
+                                    <span class="badge bg-primary">541 Like</span>
+                                    <span class="badge bg-warning">In the Vision</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="img-fluid" src="./img/2.jpeg" alt="">
+                        </div>
+                        <div class="col-9">
+                            <div class="card-body">
+                                <h5 class="card-title">Film Header</h5>
+                                <p class="card-text">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora architecto
+                                    recusandae nisi culpa? Pariatur, eius labore. Illo odio iste perferendis rem
+                                    exercitationem, voluptatibus, nisi cum porro praesentium blanditiis voluptas harum.
+                                </p>
+                                <div>
+                                    <span class="badge bg-primary">105 Comment</span>
+                                    <span class="badge bg-primary">541 Like</span>
+                                    <span class="badge bg-warning">In the Vision</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="img-fluid" src="./img/3.jpeg" alt="">
+                        </div>
+                        <div class="col-9">
+                            <div class="card-body">
+                                <h5 class="card-title">Film Header</h5>
+                                <p class="card-text">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora architecto
+                                    recusandae nisi culpa? Pariatur, eius labore. Illo odio iste perferendis rem
+                                    exercitationem, voluptatibus, nisi cum porro praesentium blanditiis voluptas harum.
+                                </p>
+                                <div>
+                                    <span class="badge bg-primary">105 Comment</span>
+                                    <span class="badge bg-primary">541 Like</span>
+                                    <span class="badge bg-warning">In the Vision</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
