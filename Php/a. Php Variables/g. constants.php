f4403e41887b4dec5114b12450f633d732d44f3a
kerimmstfdemir <kerimmstfdemir@gmail.com>
2023-04-19 09:29:39 +0300

    added php constants work

---
+++ Php/a. Php Variables/g. constants.php
@@ -0,0 +1,16 @@
+<?php
+
+    # PHP Constants
+    # Constants are used to store data, but the assigned value cannot be changed.
+
+    define("connection","mssql");
+    define("username","kerimmstfdemir");
+    const password = "1234";
+
+    // connection = "mysql";
+    
+    echo connection."<br>";
+    echo username."<br>";
+    echo password."<br>";
+
+?>
